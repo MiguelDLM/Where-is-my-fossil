b063718113948674dfdc70236300559c2a8ca4ec
Your Name <you@example.com>
2025-04-10 13:50:05 +0200

    Updated to v1.3.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2622908734"
+    serviceWorkerVersion: "2954457253"
   }
 });
